7bb106c91e15431de12a414bc2660a31438e8012
walkersutton <walker.c.sutton@gmail.com>
2021-08-18 21:59:10 -0400

    cleanup redeye and add restoreGamma hotkey

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,26 +1,21 @@
--- Redeye ----------
-function redden()
-    gamma = hs.screen.primaryScreen():getGamma()
-    descending, ascending = gamma['whitepoint']['green'], gamma['blackpoint']['red']
-    while descending > hs.battery.percentage() / 10 do
-        for key, screen in pairs(hs.screen.allScreens()) do
-            screen:setGamma({red=1, green=descending, blue=descending}, {red=ascending})
-            ascending = ascending + .005
-            descending = descending - .005
-        end
-    end
-end
-
-hs.battery.watcher.new( function()
+function redeye()
     if (hs.battery.percentage() < 10.0) and (not hs.battery.isCharging()) then
-        redden()
+        gamma = hs.screen.primaryScreen():getGamma()
+        descending, ascending = gamma['whitepoint']['green'], gamma['blackpoint']['red']
+        while descending > hs.battery.percentage() / 10 do
+            for key, screen in pairs(hs.screen.allScreens()) do
+                screen:setGamma({red=1, green=descending, blue=descending}, {red=ascending})
+                ascending = ascending + .005
+                descending = descending - .005
+            end
+        end
     else
         hs.screen.restoreGamma()
     end
-end):start()
--- end Redeye ------
+end
 
+hs.battery.watcher.new( redeye ):start()
 
-hs.hotkey.bind({'cmd', 'alt'}, 'c', function() 
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+hs.hotkey.bind({'cmd', 'alt'}, 'R', function() 
+    hs.screen.restoreGamma()
 end)
